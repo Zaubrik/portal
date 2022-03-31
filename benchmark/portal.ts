02a7bcec689cbb513817970d6132a442019a97c7
timonson <timo.rossa@tutanota.com>
2022-03-31 15:22:08 +0200

    Update

--- benchmark/portal.ts
+++ benchmark/portal.ts
@@ -1,6 +1,6 @@
 import { Portal } from "../portal.ts";
 
 const router = new Portal();
-router.get({ pathname: "/" }, (_req) => new Response("Hello"));
+router.get("http://localhost:1234/", (_req) => new Response("Hello"));
 
 router.listen({ port: 1234 });
