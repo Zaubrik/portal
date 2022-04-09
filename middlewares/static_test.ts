56ad61d3c6807c805404b14cb3fba370d02573c4
timonson <timo.rossa@tutanota.com>
2022-04-09 21:29:39 +0200

    Fix test

--- middlewares/static_test.ts
+++ middlewares/static_test.ts
@@ -5,10 +5,10 @@ import { assertEquals, getResponseTextFromApp } from "../test_deps.ts";
 Deno.test("[static] overview", async function () {
   const app = new Portal();
   const getResponseText = getResponseTextFromApp(app);
-  app.get({ pathname: "*" }, serveStatic(new URL(import.meta.url)));
+  app.get({ pathname: "*" }, serveStatic(new URL("./", import.meta.url)));
   assertEquals(
     await getResponseText(
-      new Request(new URL(import.meta.url).href),
+      new Request(`https://example.com/${import.meta.url.split("/").pop()}`),
     ),
     await Deno.readTextFile(new URL(import.meta.url)),
   );
