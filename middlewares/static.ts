22813c1468f1cf7466dc1f1c121ef7a25afb7d93
timonson <timo.rossa@tutanota.com>
2022-04-12 01:15:35 +0200

    Add option appendTrailingSlash for paths to directories

--- middlewares/static.ts
+++ middlewares/static.ts
@@ -4,6 +4,7 @@ import { fromFileUrl, join, serveFile } from "./deps.ts";
 type ServeFileOptions = {
   home?: string;
   enableCors?: boolean;
+  appendTrailingSlash?: boolean;
   subdomainGroup?: string;
 };
 
@@ -29,19 +30,29 @@ function setCors(res: Response): void {
  */
 export function serveStatic(
   root: URL | string,
-  { home = "index.html", enableCors = false, subdomainGroup }:
-    ServeFileOptions = {},
+  {
+    home = "index.html",
+    enableCors = false,
+    appendTrailingSlash = true,
+    subdomainGroup,
+  }: ServeFileOptions = {},
 ) {
   const rootStr = root instanceof URL ? fromFileUrl(root) : root;
   return async (ctx: Context) => {
     if (ctx.response.ok) return ctx.response;
+    const pathname = ctx.url.pathname;
     const subdomainStr = subdomainGroup
       ? ctx.urlPatternResult.hostname.groups[subdomainGroup]
         .replaceAll(".", "/")
       : "";
-    const pathname = ctx.url.pathname;
     const absolutePath = join(rootStr, subdomainStr, pathname);
     const fileInfo = await Deno.stat(absolutePath);
+    if (
+      appendTrailingSlash && fileInfo.isDirectory &&
+      absolutePath.slice(-1) !== "/"
+    ) {
+      throw Response.redirect(ctx.request.url + "/", 301);
+    }
     const filePath = fileInfo.isDirectory
       ? join(absolutePath, home)
       : absolutePath;
