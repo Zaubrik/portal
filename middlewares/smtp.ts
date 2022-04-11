2105078fdff5217b42c2401a179438fc94fd165c
timonson <timo.rossa@tutanota.com>
2022-04-12 01:15:39 +0200

    smtp_test.ts

--- middlewares/smtp.ts
+++ middlewares/smtp.ts
@@ -9,11 +9,7 @@ import {
 
 export { quotedPrintableEncode, SmtpClient };
 export type { ConnectConfig, ConnectConfigWithAuthentication, SendConfig };
-export type Options = {
-  isTls?: boolean;
-  headers?: Headers;
-  formatter?: (obj: Record<string, unknown>) => string;
-};
+export type Options = { isTls?: boolean; headers?: Headers };
 
 const client = new SmtpClient();
 
@@ -27,10 +23,10 @@ function isObjectWide(obj: unknown): obj is Record<string, unknown> {
 export function send(
   connectConfig: ConnectConfig | ConnectConfigWithAuthentication,
   sendConfigOrCb: SendConfig | {
-    cb: ((id: string) => SendConfig);
+    cb: ((id: string, data: Record<string, unknown>) => SendConfig);
     idGroup: string;
   },
-  { isTls = true, headers = new Headers(), formatter }: Options = {},
+  { isTls = true, headers = new Headers() }: Options = {},
 ) {
   return async (ctx: Context) => {
     try {
@@ -41,18 +37,15 @@ export function send(
         throw new Error("Using a callback requires a group match.");
       }
       const body = await ctx.request.text();
-      const sendConfigWithoutBody = "idGroup" in sendConfigOrCb
-        ? sendConfigOrCb.cb(
-          ctx.urlPatternResult.pathname.groups[sendConfigOrCb.idGroup],
-        )
-        : sendConfigOrCb;
-      const data = JSON.parse(body);
-      if (isObjectWide(data)) {
-        const sendConfig = {
-          ...sendConfigWithoutBody,
-          content: formatter ? formatter(data) : body,
-        };
+      const bodyData = JSON.parse(body);
+      if (isObjectWide(bodyData)) {
         try {
+          const sendConfig = "idGroup" in sendConfigOrCb
+            ? sendConfigOrCb.cb(
+              ctx.urlPatternResult.pathname.groups[sendConfigOrCb.idGroup],
+              bodyData,
+            )
+            : { ...sendConfigOrCb, content: body };
           if (isTls) {
             await client.connectTLS(connectConfig);
           } else {
@@ -65,7 +58,7 @@ export function send(
           throw new Response("Internal Server Error", { status: 500 });
         }
       } else {
-        throw new Error("The data must be an object.");
+        throw new Error("The body's data must be an object.");
       }
     } catch (err) {
       if (err instanceof Response) {
