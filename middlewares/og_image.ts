6e045dec913801a0f3ba757a6894ed3984c21967
timonson <timo.rossa@tutanota.com>
2022-04-21 18:59:18 +0200

    Add caching

--- middlewares/og_image.ts
+++ middlewares/og_image.ts
@@ -2,19 +2,20 @@ import { createOgImage } from "./deps.ts";
 import { Context } from "../portal.ts";
 
 /**
- * This middleware generates dynamic Open Graph images that you can embed in your
- * html meta tags.
+ * This middleware generates dynamic Open Graph images that you can embed
+ * in your html meta tags. Either it returns a `Response` with `Cache-Control`
+ * headers or it throws an `Error`.
  * ```ts
  * app.get({ pathname: "/{:text}.png" }, serveOgImage);
  * ```
  */
 export async function serveOgImage(ctx: Context): Promise<Response> {
-  try {
-    const canvas = await createOgImage(ctx.request);
-    return new Response(canvas.toBuffer(), {
-      headers: { "content-type": "image/png" },
-    });
-  } catch {
-    throw new Response("Bad Request", { status: 400 });
-  }
+  const canvas = await createOgImage(ctx.request);
+  return new Response(canvas.toBuffer(), {
+    headers: {
+      "content-type": "image/png",
+      "Cache-Control":
+        `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`,
+    },
+  });
 }
