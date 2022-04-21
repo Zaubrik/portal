233fddd6101b768c1c06c742ae6b153fff8d5c4b
timonson <timo.rossa@tutanota.com>
2022-04-21 19:07:03 +0200

    Remove 'refresh' middleware

--- url_pattern_ui/server.ts
+++ url_pattern_ui/server.ts
@@ -1,13 +1,7 @@
 import { Portal, serveStatic } from "../mod.ts";
-import { refresh } from "https://deno.land/x/refresh@1.0.0/mod.ts";
 
 const app = new Portal();
-const refreshMiddleware = refresh();
 
-app.use((ctx) => {
-  const res = refreshMiddleware(ctx.request);
-  if (res) throw res;
-});
 app.get({ pathname: "*" }, serveStatic(new URL("./", import.meta.url)));
 
 await app.listen({ port: 8080 });
