87db9b7d71dd97f29d96391d01bc853b023b864a
timonson <timo.rossa@tutanota.com>
2022-04-24 17:46:22 +0200

    Fix

--- middlewares/log.ts
+++ middlewares/log.ts
@@ -59,7 +59,7 @@ export async function logger(
 ) {
   await log.setup(getConfig(configOrUrlToLogFile));
   const logger = log.getLogger();
-  return async (ctx: Context): Promise<void> => {
+  return (ctx: Context): Promise<void> => {
     if (!ctx.response.ok && isBetween(ctx.response.status, 500, 599)) {
       logger.critical(createMessage(ctx));
     } else if (ctx.error) {
