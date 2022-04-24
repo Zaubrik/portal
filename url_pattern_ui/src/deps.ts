38276e60d8d4ec78aa337c23b3ac3083ca01cd36
timonson <timo.rossa@tutanota.com>
2022-04-24 17:45:36 +0200

    Update

--- url_pattern_ui/src/deps.ts
+++ url_pattern_ui/src/deps.ts
@@ -1,2 +1,2 @@
-export { LabeledControls } from "https://deno.land/x/pouch@v0.0.3/mod.ts";
+export { LabeledControls } from "https://deno.land/x/pouch@v0.0.5/mod.ts";
 export { SvgButton } from "/home/ubustreet/Workspace/projects/generator/web_components/svgButton/svgButton.ts";
