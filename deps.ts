48c0b79c1af59568794184c188281defc3aba6bc
timonson <timo.rossa@tutanota.com>
2022-06-08 18:04:44 +0200

    Bump

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export { serve, serveTls } from "https://deno.land/std@0.137.0/http/server.ts";
+export { serve, serveTls } from "https://deno.land/std@0.142.0/http/server.ts";
 
 export type {
   ConnInfo,
   ServeInit,
   ServeTlsInit,
-} from "https://deno.land/std@0.137.0/http/server.ts";
+} from "https://deno.land/std@0.142.0/http/server.ts";
--- middlewares/deps.ts
+++ middlewares/deps.ts
@@ -1,13 +1,14 @@
-export { fromFileUrl, join } from "https://deno.land/std@0.137.0/path/mod.ts";
-export { serveFile } from "https://deno.land/std@0.137.0/http/file_server.ts";
+export { fromFileUrl, join } from "https://deno.land/std@0.142.0/path/mod.ts";
+export { ensureFile } from "https://deno.land/std@0.142.0/fs/mod.ts";
+export { serveFile } from "https://deno.land/std@0.142.0/http/file_server.ts";
 export { verify } from "https://deno.land/x/djwt@v2.4/mod.ts";
 export { SMTPClient } from "https://deno.land/x/denomailer@1.0.0/mod.ts";
 export { createOgImage } from "https://deno.land/x/portrait@v0.0.7/mod.ts";
-export * as log from "https://deno.land/std@0.137.0/log/mod.ts";
+export * as log from "https://deno.land/std@0.142.0/log/mod.ts";
 
 export type { Payload } from "https://deno.land/x/djwt@v2.4/mod.ts";
 export type {
   ClientOptions,
   SendConfig,
 } from "https://deno.land/x/denomailer@1.0.0/mod.ts";
-export type { LogConfig } from "https://deno.land/std@0.137.0/log/mod.ts";
+export type { LogConfig } from "https://deno.land/std@0.142.0/log/mod.ts";
