1d70b0d2eedb28c0c40eef56c8744cd0594ae6db
timonson <timo.rossa@tutanota.com>
2022-10-31 22:13:44 +0100

    -

--- test_deps.ts
+++ test_deps.ts
@@ -3,8 +3,8 @@ export {
   assertNotEquals,
   assertRejects,
   assertThrows,
-} from "https://deno.land/std@0.160.0/testing/asserts.ts";
-export { delay } from "https://deno.land/std@0.160.0/async/delay.ts";
+} from "https://deno.land/std@0.161.0/testing/asserts.ts";
+export { delay } from "https://deno.land/std@0.161.0/async/delay.ts";
 export {
   compose,
   composeSync,
@@ -20,12 +20,12 @@ export {
   type Payload,
   verify,
 } from "https://deno.land/x/djwt@v2.7/mod.ts";
-export { identity } from "https://dev.zaubrik.com/sorcery@v0.0.6/higher_order.js";
+export { identity } from "https://dev.zaubrik.com/sorcery@v0.0.7/higher_order.js";
 export {
   isNull,
   isPresent,
   isString,
-} from "https://dev.zaubrik.com/sorcery@v0.0.6/type.js";
+} from "https://dev.zaubrik.com/sorcery@v0.0.7/type.js";
 
 export const connInfo = {
   localAddr: { transport: "tcp" as const, hostname: "127.0.0.1", port: 8080 },
