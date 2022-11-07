2dfa72f537647e54e9456872a02b7875803b0d4e
timonson <timo.rossa@tutanota.com>
2022-11-07 19:22:45 +0100

    -

--- middlewares/fetch_test.ts
+++ middlewares/fetch_test.ts
@@ -7,9 +7,6 @@ const ctx = new Context(new Request(`https://example.com/`), connInfo);
 Deno.test("overview", async function () {
   const returnedStatus = await allAndEverythingRoute(
     fetchResponse({ hostname: "_" }),
-  )(ctx).catch((error) => error.message);
-  assertEquals(
-    await returnedStatus,
-    "error sending request for url (https://_/): error trying to connect: dns error: failed to lookup address information: Name or service not known",
-  );
+  )(ctx).catch((error) => error);
+  assertEquals(returnedStatus instanceof Error, true);
 });
