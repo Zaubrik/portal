0aff31d8d076543301a4e8ab977f5a33f0bcf9e0
timonson <timo.rossa@tutanota.com>
2023-06-15 00:20:23 +0200

    -

--- middlewares/mod.ts
+++ middlewares/mod.ts
@@ -4,7 +4,6 @@ export * from "./error_fallback.ts";
 export * from "./fetch.ts";
 export * from "./webhook.ts";
 export * from "./log.ts";
-export * from "./og_image.ts";
 export * from "./smtp.ts";
 export * from "./static.ts";
 export * from "./subdomain.ts";
