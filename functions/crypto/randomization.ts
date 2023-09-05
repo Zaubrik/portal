5b0cac36df3b111ed326b41d5bb25f4fb27023ed
timonson <timo.rossa@tutanota.com>
2023-09-05 02:15:23 +0200

    -

--- functions/crypto/randomization.ts
+++ functions/crypto/randomization.ts
@@ -1,15 +1,41 @@
 /**
  * generateId
  * Produces a random 8-character hexadecimal string.
+ * @param {number} [length=10]
  * @returns {string}
  */
-export function generateId() {
-  const hexString = crypto.getRandomValues(new Uint32Array(1))[0].toString(16);
-  return hexString.padStart(8, "0").slice(0, 8);
+export function generateId(length = 10) {
+  const minHexChars = 2; // Minimum number of characters required for a valid hex digit
+  const maxBytes = 65536; // Maximum number of random bytes available
+  const maxHexChars = maxBytes * 2; // Each byte corresponds to 2 hex characters
+
+  if (length < minHexChars) {
+    // If the length is too small, generate a random number instead
+    const randomHex = Math.floor(Math.random() * Math.pow(16, minHexChars))
+      .toString(16).padStart(minHexChars, "0");
+    return randomHex;
+  }
+
+  if (length > maxHexChars) {
+    throw new Error(
+      `Requested length exceeds the maximum allowable (${maxHexChars} characters).`,
+    );
+  }
+
+  const bytes = new Uint8Array(length / 2);
+  crypto.getRandomValues(bytes);
+
+  const hexString = Array.from(
+    bytes,
+    (byte) => byte.toString(16).padStart(2, "0"),
+  ).join("");
+
+  return hexString;
 }
 
 /**
  * generateSalt.
+ * @param {number} [length=16]
  * @returns {Uint8Array}
  */
 export function generateSalt(length = 16) {
